dcdb1d2bb9f7affb1d8a338e685488529693e55b
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-03 01:57:32 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,7 +5,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :name
       t.string :location
       t.string :theme
-      t.integer :num_of_employees
+      t.integer :price
       t.boolean :still_in_business
       t.string :opening_time
       t.string :closing_time
