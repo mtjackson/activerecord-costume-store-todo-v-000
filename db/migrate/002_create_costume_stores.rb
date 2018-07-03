d4cb7907d54feec263aa2023a734e7d74ddbd9f9
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-03 01:55:43 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,6 +8,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.integer :num_of_employees
       t.boolean :still_in_business
       t.string :opening_time
+      t.string :closing_time
     end
   end
 
