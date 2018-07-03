619453c71551b1ca045d723be4b3cdbb8dfd992c
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-03 01:53:48 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,8 +3,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_store do |t|
       t.string :name
-      t.string :price
-      t.string :image_url
+      t.string :location
+      t.integer :costume_inventory
       t.integer :size
     end
   end
